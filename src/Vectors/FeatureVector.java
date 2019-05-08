5a466515c43ef26b69dd94bc6a365199fcdcb4ea
Brandon Sorensen <sorensen.12@gmail.com>
2019-05-08 21:29:43 +0200

    Added default cosine similarity method.

--- src/Vectors/FeatureVector.java
+++ src/Vectors/FeatureVector.java
@@ -1,30 +1,54 @@
 package Vectors;
 
-public interface FeatureVector {
+interface FeatureVector {
 
+    /**
+     * Calculates the inner dot product of two <code>FeatureVector</code> objects.
+     * @param other another vector
+     * @return the dot product of two vectors
+     */
     double dot(FeatureVector other);
 
-    double cosineSimilarity(FeatureVector other);
+    /**
+     * Calculates the cosine similarity of two vectors.
+     * @param other another vector
+     * @return the cosine similarity of this and another vector
+     */
+    default double cosineSimilarity(FeatureVector other) {
+        double dotProduct = dot(other);
+        double euclideanLengthA = pow(2).sum();
+        double euclideanLengthB = other.pow(2).sum();
 
-    FeatureVector add(FeatureVector other);
+        return dotProduct / (Math.sqrt(euclideanLengthA) * Math.sqrt(euclideanLengthB));
+    }
+
+    FeatureVector add(CompressedFeatureVector other);
+
+    FeatureVector add(SparseFeatureVector other);
 
     FeatureVector add(int scalar);
 
     FeatureVector add(double scalar);
 
-    FeatureVector subtract(FeatureVector other);
+    FeatureVector subtract(CompressedFeatureVector other);
+
+    FeatureVector subtract(SparseFeatureVector other);
 
     FeatureVector subtract(int scalar);
 
     FeatureVector subtract(double scalar);
 
-    FeatureVector multiply(FeatureVector other);
+    FeatureVector multiply(CompressedFeatureVector other);
+
+    FeatureVector multiply(SparseFeatureVector other);
 
     FeatureVector multiply(int scalar);
 
     FeatureVector multiply(double scalar);
 
-    FeatureVector divide(FeatureVector other);
+    FeatureVector divide(CompressedFeatureVector other);
+
+    FeatureVector divide(SparseFeatureVector other);
 
     FeatureVector divide(int scalar);
 
@@ -33,6 +57,12 @@ public interface FeatureVector {
     FeatureVector pow(int scalar);
 
     FeatureVector pow(double scalar);
+
+    double sum();
+
+    double product();
+
+    int getLength();
 
     double get(int index);
 
