f62aa1cc58deac25a1172bafaf32dd34fa8d4847
Brandon Sorensen <sorensen.12@gmail.com>
2019-05-09 22:58:17 +0200

    Added IsearDataLoader unfilled.

---
+++ src/IsearDataLoader.java
@@ -0,0 +1,19 @@
+import java.util.Iterator;
+import java.util.List;
+
+public class IsearDataLoader implements Iterator<String> {
+
+    private List<String[]> readCSV(String path) {
+        return null;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public String next() {
+        return null;
+    }
+}
