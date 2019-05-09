b084b979e346917277d633e53e852691dc8b6354
Brandon Sorensen <sorensen.12@gmail.com>
2019-05-09 22:52:57 +0200

    Added WeightVector subclass.

---
+++ src/Vectors/WeightVector.java
@@ -0,0 +1,26 @@
+package Vectors;
+
+public class WeightVector extends SparseFeatureVector {
+
+    private double bias;
+
+    WeightVector(int length) {
+        this(length, 0.0);
+    }
+
+    WeightVector(int length, double bias) {
+        super(length);
+        this.bias = bias;
+    }
+
+    WeightVector(double[] vector, double bias) {
+        super(vector);
+        this.bias = bias;
+    }
+
+    WeightVector(double[] vector) {
+        this(vector, 0.0);
+    }
+
+
+}
