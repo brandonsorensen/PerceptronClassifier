889e70a6dbd1a4d86731a11e66928cf515bc21f4
Brandon Sorensen <sorensen.12@gmail.com>
2019-05-10 15:01:26 +0200

    Added randomInitialize methods from compressed and sparse vectors.

--- src/Vectors/FeatureMatrix.java
+++ src/Vectors/FeatureMatrix.java
@@ -6,14 +6,14 @@ public class FeatureMatrix extends AbstractCollection<FeatureVector> {
     private FeatureVector[] matrix;
     private final int[] shape;
 
-    public FeatureMatrix(Collection<FeatureVector> vectors) throws VectorLengthException {
+    public FeatureMatrix(Collection<FeatureVector> vectors) {
         matrix = new FeatureVector[vectors.size()];
         vectors.toArray(matrix);
         ensureLength(matrix);
         shape = new int[] {matrix[0].size(), matrix.length};
     }
 
-    public FeatureMatrix(FeatureVector[] vectors) throws VectorLengthException {
+    public FeatureMatrix(FeatureVector[] vectors) {
         ensureLength(vectors);
         matrix = vectors;
         shape = new int[] {matrix[0].size(), matrix.length};
@@ -84,7 +84,7 @@ public class FeatureMatrix extends AbstractCollection<FeatureVector> {
         }
     }
 
-    private void ensureLength(FeatureVector[] matrix) throws VectorLengthException {
+    private void ensureLength(FeatureVector[] matrix) {
         int firstLength = matrix[0].size();
         for (FeatureVector vector : matrix) {
             if (vector.size() != firstLength)
@@ -104,8 +104,52 @@ public class FeatureMatrix extends AbstractCollection<FeatureVector> {
         return shape[1];
     }
 
+    public FeatureVector get(int i) {
+        if (i < 0 || i > size())
+            throw new IndexOutOfBoundsException();
+        return matrix[i];
+    }
+
     public int[] getShape() {
         return shape;
+    }
+
+    /**
+     * Initializes a <code>FeatureMatrix</code> object with random values of the shape
+     * {nFeatures, length}. Uses the <code>SparseFeatureVector</code> implementation.
+     * @param length the number of vectors in the matrix
+     * @param nFeatures the length of each vector
+     * @return a matrix of shape {nFeatures, length}
+     */
+    public static FeatureMatrix randomInitialize(int nFeatures, int length) {
+        if (length < 0)
+            throw new IllegalArgumentException("Length must be a positive integer.");
+
+        SparseFeatureVector[] matrix = new SparseFeatureVector[length];
+        for (int i = 0; i < length; i++) {
+            matrix[i] = SparseFeatureVector.randomInitialize(nFeatures);
+        }
+        return new FeatureMatrix(matrix);
+    }
+
+    /**
+     * Initializes a <code>FeatureMatrix</code> object with random values of the shape
+     * {nFeatures, length}. Uses the <code>CompressedFeatureVector</code> implementation.
+     * @param length the number of vectors in the matrix
+     * @param nFeatures the length of each vector
+     * @param sparsity the level of sparsity of the array: higher values mean fewer non-
+     *                 zero elements in the array. Must be between 0 and 1.
+     * @return a matrix of shape {nFeatures, length}
+     */
+    public static FeatureMatrix randomInitialize(int nFeatures, int length, double sparsity) {
+        if (length < 0)
+            throw new IllegalArgumentException("Length must be a positive integer.");
+
+        CompressedFeatureVector[] matrix = new CompressedFeatureVector[length];
+        for (int i = 0; i < length; i++) {
+            matrix[i] = CompressedFeatureVector.randomInitialize(nFeatures, sparsity);
+        }
+        return new FeatureMatrix(matrix);
     }
 }
 
