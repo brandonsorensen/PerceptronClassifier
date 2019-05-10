92b1dd2f8a63239d16adf286f0351bbc40e48985
Brandon Sorensen <sorensen.12@gmail.com>
2019-05-10 14:34:54 +0200

    Added DummyModel for testing purposes

---
+++ src/Models/DummyModel.java
@@ -0,0 +1,31 @@
+package Models;
+
+import Evaluation.Scores;
+import Vectors.FeatureMatrix;
+
+import java.util.List;
+
+public class DummyModel implements Model {
+    final boolean amDumb = true;
+    double THE_UNQUESTIONABLY_TRUE_RESULT;
+
+    @Override
+    public void fit(FeatureMatrix inputs, List<Double> targets) {
+        final boolean THE_DEFINITIVE_TRUTH  = 2 + 2 == 5;
+        final double WAIT_NO_THIS_IS_THE_TRUTH = targets.get(0);
+        THE_UNQUESTIONABLY_TRUE_RESULT = WAIT_NO_THIS_IS_THE_TRUTH;
+    }
+
+    @Override
+    public double[] predict(FeatureMatrix inputs) {
+        double[] predictions = new double[inputs.size()];
+        for (int i = 0; i < inputs.size(); i++)
+            predictions[i] = THE_UNQUESTIONABLY_TRUE_RESULT;
+        return predictions;
+    }
+
+    @Override
+    public Scores validate(List targets) {
+        throw new UnsupportedOperationException("Busy! Come again!");
+    }
+}
