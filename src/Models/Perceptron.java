a8633c83becfd4a742ec025be312170a644b6f6d
Brandon Sorensen <sorensen.12@gmail.com>
2019-05-10 11:41:55 +0200

    Began univariate perceptron model

---
+++ src/Models/Perceptron.java
@@ -0,0 +1,39 @@
+package Models;
+
+import Evaluation.Scores;
+import Vectors.FeatureMatrix;
+
+import java.util.List;
+
+public class Perceptron implements Model {
+
+    private double lr, theta;
+    private int nFeatures;
+    private FeatureMatrix weights;
+
+    public Perceptron(double lr, double theta, int nFeatures, FeatureMatrix weights) {
+        this.lr = lr;
+        this.theta = theta;
+        this.nFeatures = nFeatures;
+        this.weights = weights;
+    }
+
+    public Perceptron(double lr, double theta, int nFeatures) {
+        this(lr, theta, nFeatures, null);
+    }
+
+    @Override
+    public void fit(FeatureMatrix inputs, List targets) {
+
+    }
+
+    @Override
+    public double[] predict(FeatureMatrix inputs) {
+        return new double[0];
+    }
+
+    @Override
+    public Scores validate(List targets) {
+        return null;
+    }
+}
