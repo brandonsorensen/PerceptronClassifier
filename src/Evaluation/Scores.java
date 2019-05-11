7a0ff205110b3b8825a00ec4f6921501e6b19ba9
Brandon Sorensen <sorensen.12@gmail.com>
2019-05-11 16:11:41 +0200

    Implemented Scores class

--- src/Evaluation/Scores.java
+++ src/Evaluation/Scores.java
@@ -1,4 +1,39 @@
 package Evaluation;
 
 public class Scores {
+    private double precision, recall, fscore;
+
+    public Scores(double precision, double recall, double fscore) {
+        this.precision = precision;
+        this.recall = recall;
+        this.fscore = fscore;
+    }
+
+    public Scores() {
+        this(0.0, 0.0, 0.0);
+    }
+
+    public double getPrecision() {
+        return precision;
+    }
+
+    public void setPrecision(double precision) {
+        this.precision = precision;
+    }
+
+    public double getRecall() {
+        return recall;
+    }
+
+    public void setRecall(double recall) {
+        this.recall = recall;
+    }
+
+    public double getFscore() {
+        return fscore;
+    }
+
+    public void setFscore(double fscore) {
+        this.fscore = fscore;
+    }
 }
